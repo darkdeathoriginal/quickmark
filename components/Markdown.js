dcb6fd50562fcdd07bf0320617a0c93fe7d197d6
Anwin Sharon <87226858+darkdeathoriginal@users.noreply.github.com>
2024-05-09 01:48:30 +0530

    fixed some bugs and added code preview style selector

--- components/Markdown.js
+++ components/Markdown.js
@@ -5,7 +5,7 @@ import { Textarea } from "./ui/textarea";
 import Md from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import * as allstyles from "react-syntax-highlighter/dist/esm/styles/prism";
 import { useToast } from "./ui/use-toast";
 import { LuCopy } from "react-icons/lu";
 import dynamic from "next/dynamic";
@@ -15,8 +15,12 @@ export default function Markdown() {
   const [inputText, setInputText] = useState("");
   const { toast } = useToast();
   const [textareaHeight, setTextareaHeight] = useState(384);
-  const textareaRef = useRef(null);
+  const [selectedStyle, setSelectedStyle] = useState("materialDark"); // Default style
+  const handleChangeStyle = (e) => {
+    setSelectedStyle(e.target.value);
+  };
 
+  const textareaRef = useRef(null);
   const handleInputChange = (e) => {
     const newText = e.target.value;
     setInputText(newText);
@@ -43,7 +47,7 @@ export default function Markdown() {
       resizeObserver.disconnect();
     };
   }, []);
-
+  const hasCode = inputText.includes("```");
   return (
     <div className="flex flex-col h-full py-5 ">
       <div className="flex lg:flex-row flex-col gap-4 lg:gap-0">
@@ -68,7 +72,7 @@ export default function Markdown() {
         <div className="flex flex-1 items-center justify-center">
           <div className="flex flex-col w-full lg:pl-5 pl-0 h-full">
             <div
-              className="w-full bg-[#0D1117] h-96 overflow-auto rounded-md p-3 border border-[#30363D] markdown"
+              className="lg:max-w-[calc(100vw-54vw)] bg-[#0D1117] h-96 overflow-auto rounded-md p-3 border border-[#30363D] markdown"
               style={{ height: textareaHeight + 17.43 }}
             >
               <Md
@@ -82,7 +86,7 @@ export default function Markdown() {
                         {...rest}
                         PreTag="div"
                         language={match[1]}
-                        style={materialDark}
+                        style={allstyles[selectedStyle]}
                       >
                         {String(children).replace(/\n$/, "")}
                       </SyntaxHighlighter>
@@ -97,6 +101,20 @@ export default function Markdown() {
                 {inputText}
               </Md>
             </div>
+            {hasCode&&<div className="flex flex-col justify-between items-center mt-3 md:flex-row">
+                <span className="text-white">Select Syntax Highlighter:</span>
+                <select
+                  value={selectedStyle}
+                  onChange={handleChangeStyle}
+                  className="bg-gray-800 text-white border-[#30363D] rounded-md px-2 py-1"
+                >
+                  {Object.keys(allstyles).map((style) => (
+                    <option key={style} value={style}>
+                      {style}
+                    </option>
+                  ))}
+                </select>
+              </div>}
           </div>
         </div>
       </div>
